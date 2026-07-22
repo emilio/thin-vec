7c840df93b210ceabc0fc244b1f5bc804cc2643c
panstromek <panstromek@seznam.cz>
2026-07-22 22:18:08 +0200

    refactor: move drop_non_singleton out of the Drop impl

--- src/lib.rs
+++ src/lib.rs
@@ -1920,23 +1920,23 @@ impl<T: PartialEq> ThinVec<T> {
     }
 }
 
+#[cold]
+#[inline(never)]
+fn drop_non_singleton<T>(this: &mut ThinVec<T>) {
+    unsafe {
+        ptr::drop_in_place(&mut this[..]);
+
+        if this.uses_stack_allocated_buffer() {
+            return;
+        }
+
+        dealloc(this.ptr() as *mut u8, layout::<T>(this.capacity()))
+    }
+}
+
 impl<T> Drop for ThinVec<T> {
     #[inline]
     fn drop(&mut self) {
-        #[cold]
-        #[inline(never)]
-        fn drop_non_singleton<T>(this: &mut ThinVec<T>) {
-            unsafe {
-                ptr::drop_in_place(&mut this[..]);
-
-                if this.uses_stack_allocated_buffer() {
-                    return;
-                }
-
-                dealloc(this.ptr() as *mut u8, layout::<T>(this.capacity()))
-            }
-        }
-
         if !self.is_singleton() {
             drop_non_singleton(self);
         }
